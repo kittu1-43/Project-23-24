bac4fd75ead20261731625fdc11db66a6e8ca91a
Your Name <you@example.com>
2022-10-06 12:39:19 +0530

    Done

---
+++ file.js
@@ -0,0 +1,50 @@
+//-------Project 23 (while loop)--------------------
+
+/*
+var count=0;
+while(count<5)
+{
+    let value = prompt("Enter the value");
+    console.log("Value : "+value);
+    count++;
+}
+
+*/
+
+//-----------Project 23 (for loop)-----------
+
+/*
+for(count=0;count<5;count++)
+{
+    let value = prompt("Enter the value");
+    console.log("Value : "+value);
+}
+
+*/
+
+
+
+//-------------Project 24 (while loop)--------------
+
+/*
+var total=0;
+var count=0;
+while(count<5)
+{
+    let marks=parseInt(prompt("Enter the marks "));
+    total=total+marks;
+    count++;
+}
+console.log("Total marks is "+total);
+*/
+
+//---------------Project 24 (for loop)-----------
+
+
+var total=0;
+for(count=0;count<5;count++)
+{
+    let marks=parseInt(prompt("Enter the marks "));
+    total=total+marks;
+}
+console.log("Total marks is "+total);
